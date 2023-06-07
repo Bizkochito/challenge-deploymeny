c16c3a3fed4aa84ad3aa79d855140697ad1f7be7
Gregoire Hupin <hupingregoire@hotmail.com>
2023-06-07 10:45:31 +0200

    Progress.

--- preprocessing/cleaning_data.py
+++ preprocessing/cleaning_data.py
@@ -1,67 +1,43 @@
 import pandas as pd
 
-df_zip = pd.read_csv("zip_to_nis.csv")
-list_zip = df_zip["zipCode"].to_list()
-list_names = df_zip["name"].to_list()
 
-dict_zip_names = {list_zip[i]: list_names[i] for _ in list_zip }
-dummies_list = pd.read_csv("df_municipality_dummies.csv").columns
+df_zip = pd.read_csv('zipcode-belgium.csv')
+df_zip = df_zip.drop_duplicates(subset='postcode', keep="first")
+list_zip = df_zip["postcode"].to_list()
+list_names = df_zip["municipality"].to_list()
 
-"""
-{
-  "data": {
-    "area": int,
-    "property-type": "APARTMENT" | "HOUSE" | "OTHERS",
-    "rooms-number": int,
-    "zip-code": int,
-    "land-area": Optional[int],
-    "garden": Optional[bool],
-    "garden-area": Optional[int],
-    "equipped-kitchen": Optional[bool],
-    "full-address": Optional[str],
-    "swimming-pool": Optional[bool],
-    "furnished": Optional[bool],
-    "open-fire": Optional[bool],
-    "terrace": Optional[bool],
-    "terrace-area": Optional[int],
-    "facades-number": Optional[int],
-    "building-state": Optional[
-      "NEW" | "GOOD" | "TO RENOVATE" | "JUST RENOVATED" | "TO REBUILD"
-    ]
-  }
-}"""
+dict_zip_names = {k: v for k, v in zip(list_zip, list_names)}
+dummies_list = pd.read_csv("df_municipality_dummies.csv").columns.to_list()
+headers = [
+"living_area",
+"terrace",
+"garden",
+"swimming_pool"]
+headers.extend(dummies_list)
 
-rename_dict ={
-    "area": "living_area",
-    "property-type": 'building_type',
-    "swimming-pool": "swimming_pool",
-    "rooms-number": int,
-    "zip-code": int,
-    "land-area": Optional[int],
-    "garden": Optional[bool],
-    "garden-area": Optional[int],
-    "equipped-kitchen": Optional[bool],
-    "full-address": Optional[str],
-    "furnished": Optional[bool],
-    "terrace-area": Optional[int],
-  }
 
 def preprocess(data: dict):
-    df = pd.DataFrame(data)
-    df.rename(columns=rename_dict, inplace=True)
+    df = pd.Series(data).to_frame().T
 
-    df_preprocessed = pd.DataFrame(columns=[
-        "living_area",
-        "terrace",
-        "garden",
-        "swimming_pool"].extend(dummies_list))
-    df_preprocessed = df_preprocessed.fillna
-    df_preprocessed['living_area'] = df["area"]
+    df_preprocessed = pd.DataFrame(columns= headers)
 
-    df_preprocessed['terrace'] = df["area"]
-    df_preprocessed['garden'] = df["area"]
-    df_preprocessed['swimming_pool'] = df["area"]
-    df_preprocessed['energy_class'] = df["area"]
-    df_preprocessed['municipality'] = df["zip-code"]
-    df_preprocessed['municipality'] = df_preprocessed['municipality'].rename(dict_zip_names)
-    df_municipality = pd.get_dummies(df_selection[["municipality"]])
+
+    # Dealing with living_area
+    df_preprocessed["living_area"] = df['area']
+    # Dealing with terraces
+    df["terrace"] = df["terrace"].replace(True, 1).fillna(0)
+    df_preprocessed["terrace"] = df[["terrace", "terrace-area"]].sum(axis=1, skipna=False).fillna(0)
+    # Dealing with garden
+    df["garden"] = df["garden"].replace(True, 1).fillna(0)
+    df["garden-area"] = df["garden-area"].fillna(0)
+    df_preprocessed["garden"] = df[["garden", "garden-area"]].sum(axis=1, skipna=False).fillna(0)
+    # Dealing with swimming_pool
+    df_preprocessed["swimming_pool"] = df["swimming-pool"].replace([True, False], [1, 0]).fillna(0)
+    # Dealing with locality
+
+    df_preprocessed[f"municipality_{dict_zip_names[df.at[0, 'zip-code']]}"] = 1
+
+    df_preprocessed = df_preprocessed.fillna(0)
+
+    return df_preprocessed
+
